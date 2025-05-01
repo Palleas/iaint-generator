201416243092bb3e35227a21cdaaa73e65d0c0f6
Romain Pouclet <romain@sound.games>
2025-05-01 08:31:40 -0700

    Copy content of canvas into clipboard

--- render.js
+++ render.js
@@ -1,7 +1,23 @@
-document.getElementById("generate").addEventListener("click", () => {
-    draw();
+// https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/write
+async function getBlobFromCanvas(canvas) {
+    return new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          resolve(blob);
+        } else {
+          reject(new Error("Canvas toBlob failed"));
+        }
+      });
+    });
+  }
+
+document.getElementById("download").addEventListener("click", () => {
+    generateAndDownload();
 })
 
+document.getElementById("copy").addEventListener("click", () => {
+    generateAndCopy();
+})
 
 document.getElementById("editable-dingus").addEventListener("keypress", event => {
     if (event.key == "Enter") {
@@ -12,10 +28,21 @@ document.getElementById("editable-dingus").addEventListener("keypress", event =>
 
 document.getElementById("editable-dingus").addEventListener("paste", event => { event.preventDefault(); });
 
-const draw = async () => {
+const getCanvasContent = async () => {
     const container = document.getElementById("conversation");
+    return await html2canvas(container)
+}
+
+const generateAndCopy = async () => {
+    const canvas = await getCanvasContent();
+    const content = await getBlobFromCanvas(canvas);
+    const data = [new ClipboardItem({ [content.type]: content })];
+    await navigator.clipboard.write(data);
+}
+
+const generateAndDownload = async () => {
     try {
-        const canvas = await html2canvas(container)
+        const canvas = await getCanvasContent();
 
         const link = document.createElement('a');
         link.download = 'html-image.png';
@@ -32,6 +59,7 @@ const switchTheme = (theme) => {
     console.log("Switching theme", {theme});
     document.getElementById("conversation").dataset.theme = theme    
 }
+
 document.querySelectorAll(".styles button").forEach(element => {
     element.addEventListener("click", (event) => switchTheme(event.target.value))
 })
