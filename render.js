59e5b87d4b8f172b9ef4be3ed3aa572e14650ffd
Romain Pouclet <romain@sound.games>
2025-05-02 22:27:14 -0700

    Setup prettier

--- render.js
+++ render.js
@@ -5,40 +5,46 @@ async function getBlobFromCanvas(canvas) {
             if (blob) {
                 resolve(blob);
             } else {
-                reject(new Error("Canvas toBlob failed"));
+                reject(new Error('Canvas toBlob failed'));
             }
         });
     });
 }
 
-document.getElementById("download").addEventListener("click", () => {
+document.getElementById('download').addEventListener('click', () => {
     generateAndDownload();
-})
-
-document.getElementById("copy").addEventListener("click", () => {
-    generateAndCopy();
-})
-
-document.getElementById("editable-dingus").addEventListener("keypress", event => {
-    if (event.key == "Enter") {
-        event.preventDefault();
-        event.target.blur();
-    }
 });
 
-document.getElementById("editable-dingus").addEventListener("paste", event => { event.preventDefault(); });
+document.getElementById('copy').addEventListener('click', () => {
+    generateAndCopy();
+});
+
+document
+    .getElementById('editable-dingus')
+    .addEventListener('keypress', (event) => {
+        if (event.key == 'Enter') {
+            event.preventDefault();
+            event.target.blur();
+        }
+    });
+
+document
+    .getElementById('editable-dingus')
+    .addEventListener('paste', (event) => {
+        event.preventDefault();
+    });
 
 const getCanvasContent = async () => {
-    const container = document.getElementById("conversation");
-    return await html2canvas(container)
-}
+    const container = document.getElementById('conversation');
+    return await html2canvas(container);
+};
 
 const generateAndCopy = async () => {
     const canvas = await getCanvasContent();
     const content = await getBlobFromCanvas(canvas);
     const data = [new ClipboardItem({ [content.type]: content })];
     await navigator.clipboard.write(data);
-}
+};
 
 const generateAndDownload = async () => {
     try {
@@ -46,29 +52,30 @@ const generateAndDownload = async () => {
 
         const link = document.createElement('a');
         link.download = 'html-image.png';
-        link.href = canvas.toDataURL("image/png");
+        link.href = canvas.toDataURL('image/png');
         link.click();
     } catch (error) {
         console.error({ error });
-        alert("An error occured!");
+        alert('An error occured!');
     }
-
-}
+};
 
 const switchTheme = (theme) => {
-    console.log("Switching theme", { theme });
-    document.getElementById("conversation").dataset.theme = theme
-}
+    console.log('Switching theme', { theme });
+    document.getElementById('conversation').dataset.theme = theme;
+};
 
-document.querySelectorAll(".styles button").forEach(element => {
-    element.addEventListener("click", (event) => {
-        if (event.target["aria-pressed"] == "true") {
+document.querySelectorAll('.styles button').forEach((element) => {
+    element.addEventListener('click', (event) => {
+        if (event.target['aria-pressed'] == 'true') {
             return;
         }
 
-        document.querySelector(".styles button[aria-pressed=true]").setAttribute("aria-pressed", "false")
-        console.log("Target", { target: event.target })
-        event.target.setAttribute("aria-pressed", "true")
-        switchTheme(event.target.value)
-    })
-})
+        document
+            .querySelector('.styles button[aria-pressed=true]')
+            .setAttribute('aria-pressed', 'false');
+        console.log('Target', { target: event.target });
+        event.target.setAttribute('aria-pressed', 'true');
+        switchTheme(event.target.value);
+    });
+});
